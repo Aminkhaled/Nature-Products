557d073ca7fdef2ff15d0f4d7a54fbbadfac765a
Amen Khaled <Aminkhaled@users.noreply.github.com>
2020-09-25 11:28:06 +0200

    we added nav,carousel,about and 3d products section

---
+++ vendor/js/custom.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    $('.carousel-item').ripples({
+        imageUrl:'../img/item-1',
+        perturbance: 0.9,
+        interactive:true,
+        resolution:500,
+        onclick:true
+    });
+
+
+    setTimeout(function (){
+        $('.loader').remove();
+    },10000)
+
+
+});
